e251a822bde843247081e49a70ca437fc31f1c89
Ansonkyk <98352095+Ansonkyk@users.noreply.github.com>
2022-02-24 16:05:29 -0500

    Done

--- main.js
+++ main.js
@@ -2,13 +2,69 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser(F,L) {
+    const User={
+        firstName: F,
+        lastName: L
+      }
+    return User;
+}
+
+function setAge(User,age){
+    const User2={
+        firstName: User.firstName,
+        lastName: User.lastName,
+        age: age
+
+    }
+  return User2;
+}
+
+function incrementAge(User){
+    User.age+=1;
+    return User;
+}
+
+function fixCar(car){
+    car.needsMaintenance=false
+    return car;
+}
+
+function addGrades(s,g){
+    s.grades=s.grades.concat(g);
+    return s;
+}
+
+function getDataType(o,k){
+    for (const [key, value] of Object.entries(o)) {
+        if (key==k){
+            return typeof value;
+        }
+      }
+    
+}
 
 
+function addTodo(t,nt){
+    t.push(nt);
+    return t ;
+}
 
+function addSong(p,n){
+    p.songs=p.songs.concat(n);
+    p.duration=p.duration+n.duration;
+    return p;
+}
 
-
-
-
+function updateReportCard(r,g){
+    r.grades=r.grades.concat(g);
+    arr=r.grades;
+    r.lowestGrade = Math.min(...arr);
+    r.highestGrade= Math.max(...arr);
+    r.averageGrade =arr.reduce((a,b) => a + b, 0) / arr.length;
+    return r
+}
+//Test
 
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
